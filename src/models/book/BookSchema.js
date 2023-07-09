cfb4c3fca7bb2f49a3a1f08b7183c46a9575c0b8
srijan <srijan.dhl21@gmail.com>
2023-07-09 14:50:35 +1000

    removed returned date from bookschema and added on burrowschema

--- src/models/book/BookSchema.js
+++ src/models/book/BookSchema.js
@@ -14,9 +14,5 @@ const bookSchema = new mongoose.Schema({
     type: Date,
     default: null,
   },
-  returnDate: {
-    type: Date,
-    default: null,
-  },
 });
 export default mongoose.model("Book", bookSchema);
